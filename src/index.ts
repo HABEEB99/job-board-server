afc593b5429bf9f497c83ae7c5856d4bf6daae54
Habeeb <habeebahmadu1@gmail.com>
2024-06-11 14:58:14 +0100

    feat: add app middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./router/user.router";
 
 const app = express();
 
 const PORT = 5000;
+
+/** MIDDLEWARES */
+app.use(express.json());
+app.use(cors());
+app.use(morgan("tiny"));
+app.disable("x-powered-by");
+
+/** API ROUTES */
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
